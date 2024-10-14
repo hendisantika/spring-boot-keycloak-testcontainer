493e46a71e6a828d64f78df8a6927880e4ec3e4f
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 13:08:19 +0700

    Add KeyCloakTestContainerTestWithDefaultSetup🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/keycloaktestcontainer/KeyCloakTestContainerTestWithDefaultSetup.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.keycloaktestcontainer;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import io.restassured.RestAssured;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-testcontainer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/10/24
+ * Time: 13.07
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Testcontainers(disabledWithoutDocker = true)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class KeyCloakTestContainerTestWithDefaultSetup {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeyCloakTestContainerTest.class.getName());
+    @Container
+    static KeycloakContainer keycloak = new KeycloakContainer();
+    @LocalServerPort
+    private int port;
+
+    @PostConstruct
+    public void init() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+}
