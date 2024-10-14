bbb3f80d4d30338fc9bf0bb8adf6484909ea8cb8
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 13:04:31 +0700

    Add getMe in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaktestcontainer/controller/UserController.java
+++ src/main/java/id/my/hendisantika/keycloaktestcontainer/controller/UserController.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.keycloaktestcontainer.controller;
 
+import id.my.hendisantika.keycloaktestcontainer.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class.getName());
+
+    @GetMapping("me")
+    public User getMe(JwtAuthenticationToken principal) {
+        LOGGER.info("Principal attributes:: {}", principal.getTokenAttributes());
+
+        String username = principal.getTokenAttributes().get("preferred_username").toString();
+    /*String name = principal.getTokenAttributes().get("name").toString();
+    String email = principal.getTokenAttributes().get("email").toString();*/
+        return new User(1L, username, "", "", "");
+    }
 
 }
