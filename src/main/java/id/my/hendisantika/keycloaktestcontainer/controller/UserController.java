422f8476b1b8cb9905926d6b1d5fb3ae50cd9469
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 13:04:04 +0700

    Add UserController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaktestcontainer/controller/UserController.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.keycloaktestcontainer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-testcontainer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/10/24
+ * Time: 13.03
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class.getName());
+
+}
